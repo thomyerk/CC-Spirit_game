120a3fc992c4326de8e93ab7ef42f534f6f77dd7
TOM-ATIV9\Tom <thomyerk@gmail.com>
2020-12-08 15:14:05 +0100

    tile creation added

--- game.js
+++ game.js
@@ -2,6 +2,58 @@ initGame();
 
 function initGame() {
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
-
 }
+
+// function cardConstructor(cardElement, itemProperty, iconType, symbolCount){
+    
+//     return tile
+// }
+for(let item in tiles){
+    console.log(item);
+    let spiritTile = document.createElement("div")
+    spiritTile.classList.add("card")
+    // if(sorszám in (1,12,13,25,26,38,39,48))
+    //     spiritTile.onedge = true
+    let iconsContainer = spiritTile.createElement("div")
+    console.log(iconsContainer);
+    // let symbols spiritTile.insertAdjacentElement('beforeend', cardConstructor("symbols",item.spiritSymbol,type))
+    // //let symbols = cardConstructor("symbols",item.spiritSymbol,type)
+    // let icon = cardConstructor("icon",item.powerIcon,null)
+    // let howMany = cardConstructor("howMany",item.howMany,type)
+    document.body.appendChild(spiritTile)
+}
+
+
+
+var tiles = 
+[
+    {
+        "type": 'webspiders',
+        "howMany": 10,
+        "spiritSymbol": 2,
+        "powerIcon": 'sun',
+        "draggable": false,
+        "onEdge": false,
+        "favor": false
+    },
+    {
+        "type": 'webspiders',
+        "howMany": 10,
+        "spiritSymbol": 2,
+        "powerIcon": 'sun',
+        "draggable": false,
+        "onEdge": false,
+        "favor": false
+    },
+    {
+        "type": 'webspiders',
+        "howMany": 10,
+        "spiritSymbol": 2,
+        "powerIcon": 'sun',
+        "draggable": false,
+        "onEdge": false,
+        "favor": false
+    }
+]
+
+console.log(tiles);
